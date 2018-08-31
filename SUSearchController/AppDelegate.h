7966aeac4eaafac2df77b9417b010e0b2ca92d07
Superman <703353105@qq.com>
2018-08-31 13:33:01 +0800

    Initial Commit

---
+++ SUSearchController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SUSearchController
+//
+//  Created by Superman on 2018/8/31.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SUSearchController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SUSearchController
+//
+//  Created by Superman on 2018/8/31.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
