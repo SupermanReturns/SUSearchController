f03b08c964d9f39b8c9f2fa1a858ae278bb236da
Superman <703353105@qq.com>
2018-09-14 10:36:52 +0800

    修改代码

--- SUSearchController/JDSearch/JDSearchVC.m
+++ SUSearchController/JDSearch/JDSearchVC.m
@@ -8,12 +8,49 @@
 
 #import "JDSearchVC.h"
 
-@interface JDSearchVC ()
+@interface JDSearchVC (){
+    JDSearchBar *_searchBar;
+    UIViewController *_searchResultsController;
+}
 
+#define SCREENSIZE [[UIScreen mainScreen] bounds].size
+#define IPhoneX (SCREENSIZE.width == 375.f && SCREENSIZE.height == 812.f ? YES : NO)
 @end
 
 @implementation JDSearchVC
-
+-(JDSearchBar *)searchBar {
+    if (!_searchBar) {
+        _searchBar=[[JDSearchBar alloc]init];
+        [_searchBar addTarget:self action:@selector(textChange) forControlEvents:UIControlEventEditingChanged];
+    }
+    return _searchBar;
+}
+-(void)textChange{
+    if (self.searchBar.text.length ==1 &&self.delegate) {
+        [self.delegate didPresentSearchController:self];
+    }
+    if (self.searchBar.text.length == 0 &&self.delegate) {
+        [self.delegate didDismissSearchController:self];
+    }
+    if (self.searchResultsUpdater) {
+        [self.searchResultsUpdater updateSearchResultsForSearchController:self];
+    }
+}
+-(instancetype)initWithSearchResultsController:(UIViewController *)searchResultsController{
+    self=[super init];
+    if (searchResultsController) {
+        _searchResultsController=searchResultsController;
+        CGFloat yCoordinate =64.f;
+        if (IPhoneX) {
+            yCoordinate=88.f;
+        }
+        self.view.frame=CGRectMake(0, yCoordinate, SCREENSIZE.width, SCREENSIZE.height);
+        [self addChildViewController:_searchResultsController];
+        _searchResultsController.view.frame=searchResultsController.view.bounds;
+        [self.view addSubview:_searchResultsController.view];
+    }
+    return self;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
